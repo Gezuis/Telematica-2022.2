f655b7955e016987d12a7ce07792a5149399aa3c
Gezuis <111437223+Gezuis@users.noreply.github.com>
2022-09-27 11:35:49 -0300

    Update Numeros pares multiplos de 4.cpp

--- Numeros pares multiplos de 4.cpp
+++ Numeros pares multiplos de 4.cpp
@@ -12,5 +12,5 @@ int contador = 4;
 
              printf("ACABOU !!!!\n");
              system("title Numeros pares multiplos de 4");
-          
+          system(" start chrome.exe https://github.com/Gezuis/Telematica-2022.2/edit/main/Numeros%20pares%20multiplos%20de%204.cpp");
 		 }
