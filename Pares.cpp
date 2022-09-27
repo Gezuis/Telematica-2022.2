caee95558c3308f422dda863a7f0b76aa24de4da
Gezuis <111437223+Gezuis@users.noreply.github.com>
2022-09-27 11:32:53 -0300

    Add files via upload

---
+++ Pares.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <Windows.h>
+ int main(){
+int contador = 4;
+
+             while( contador < 41 )
+             {
+               printf( "contador = %d\n", contador);
+               contador += 4;
+             }
+
+             printf("ACABOU !!!!\n");
+             system("title Numeros pares multiplos de 4");
+          
+		 }
