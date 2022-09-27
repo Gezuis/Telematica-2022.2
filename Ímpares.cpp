b605ae31fe85e06ce0ef9e88307f4a24158e2cb9
Gezuis <111437223+Gezuis@users.noreply.github.com>
2022-09-27 10:52:56 -0300

    Add files via upload

---
+++ Ímpares.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <Windows.h>
+ int main(){
+int contador;
+	
+             for( contador = 5; contador < 51; contador += 5 )
+                 printf( "contador = %d\n", contador );
+
+             printf("ACABOU !!!!\n");
+             system("title Numeros impares multiplos de 5");
+         }
