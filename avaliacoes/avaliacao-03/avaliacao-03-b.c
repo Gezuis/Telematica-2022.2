648327769ae35065283defd4e794c9178dea7b91
Gezuis <111437223+Gezuis@users.noreply.github.com>
2022-12-22 12:16:08 -0300

    Update avaliacao-03-b.c

--- avaliacoes/avaliacao-03/avaliacao-03-b.c
+++ avaliacoes/avaliacao-03/avaliacao-03-b.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+
+// Função myFunction
+// Tipo - void
+// Nome - myFunction
+// Parametro - char name[]  
+// name é um parametro
+
+int meuTamanho(char frase[]) {
+ int tam = 0;
+ int i;
+ for(i=17; frase[i]; i--) {
+ printf("%d %c\n", i, frase [i]);
+ }
+ 
+ tam=i;
+ return tam;
+}
+
+int main() {
+
+  int size;
+ 
+  char s1 []= "Jose Maria e Jesus";
+ 
+  size=meuTamanho(s1);
+  printf("O Tamanho da String=%d\n", size);
+ 
+  return 0;
+}
