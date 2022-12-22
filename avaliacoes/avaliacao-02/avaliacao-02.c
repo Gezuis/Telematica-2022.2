64f5351bf62ec3bc492a7c417f63f674bb1921a8
Gezuis <111437223+Gezuis@users.noreply.github.com>
2022-12-22 12:00:35 -0300

    Update avaliacao-02.c

--- avaliacoes/avaliacao-02/avaliacao-02.c
+++ avaliacoes/avaliacao-02/avaliacao-02.c
@@ -1,3 +1,4 @@
+/*
 # Telematica-2022.2 (21/09/2022)
 Evoluindo na programação
 DEPARTAMENTO DE TELEMÁTICA  
@@ -6,6 +7,7 @@ CURSO TELEMÁTICA / DISCIPLINA: LÓGICA E LINGUAGEM DE PROGRAMAÇÃO  TEMA: ESTR
 1ª QUESTÃO: As estruturas de repetição na linguagem C, também chamadas de laço permitem  executar comandos que se repetem sob uma determinada condição. São três comandos de  repetição: for, while e do while. Responda:  
 a) Qual a diferença entre o while e o do .. while; While - se repete quando a condição é verdadeira,  quando for falsa, pula a linha do laço. Do while – se repete até que a condição seja falsa. b) No comando for há três expressões que definem como o laço executa. São: a inicialização da  
 variável de controle, o teste realizado com a variável de controle e alteração que é feita na  variável de controle. Escreva um programa em C onde é mostrado os valores de inteiros pares  e múltiplos de 5. 
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
